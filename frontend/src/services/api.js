59e12fe271ba068c6d9af17954ef575f48a6ffe4
carinatav <clarianocarina1098@gmail.com>
2025-12-10 10:33:36 -0300

    fix: usar backend em produção na API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://smpm-backend.onrender.com'
+  baseURL: 'https://smpm-backend.onrender.com/api',
 })
 
 export default api
